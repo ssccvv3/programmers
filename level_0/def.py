100f1dd4f2a9546fd5aa56d4727c12f1c73694f6
ssccvv3 <119987000+ssccvv3@users.noreply.github.com>
2023-09-08 17:59:22 +0900

    Lv_0 exercise 1-7
    exercise 1-7

---
+++ level_0/def.py
@@ -0,0 +1,15 @@
+# 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 곱을 return 하도록
+# solution 함수를 완성해주세요.
+
+
+# def solution(num1, num2):
+#     return num1*num2
+# print(solution(a,b))
+
+def solution(num1, num2):
+    answer = num1*num2
+    return answer
+
+# def add(a, b): 
+#     result = a + b 
+#     return result
